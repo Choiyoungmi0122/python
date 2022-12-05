1d2852cd593dcc93a12b5d395f4f9b72341f8cba
Choiyoungmi <choiyoungmi2252@gmail.com>
2022-12-05 19:32:05 +0900

    oc

---
+++ p02_변수/StudentInfo.py
@@ -0,0 +1,17 @@
+studentCode = 0
+studentName = ""
+studentYear = 1
+studentAge = 0
+studentAddress = ""
+
+print("학생정보 입력되는 부분")
+
+studentCode = 20212252
+studentName = "최영미"
+studentYear = 1
+studentAge = 21
+studentAddress = "부산광역시"
+
+print("학생정보 출력되는 부분")
+
+print(studentCode, studentName, studentYear + 1, studentAge, studentAddress)
