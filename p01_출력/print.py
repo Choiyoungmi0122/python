a02cda83ce72f97573186dd0818916caf87a6eb1
Choiyoungmi <choiyoungmi2252@gmail.com>
2022-12-05 19:26:02 +0900

    print

---
+++ p01_출력/print.py
@@ -0,0 +1,12 @@
+print("2 X 1 = 2", end="\t")
+print("2 X 2 = 4")
+
+year = "2022"
+month = "07"
+day = "15"
+hour = "16"
+minute = "42"
+second = "30"
+#2022/07/15 16:42:30
+print(year, month, day, sep="/", end=" ")
+print(hour, minute, second, sep=":")
